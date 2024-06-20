ed47e6bb621310a049f2279cec94e9135ac8062c
jamesmcdonald112 <jamesmcdonald112@yahoo.ie>
2024-06-20 12:17:25 +0100

    Completed stage 3/5. Calculated the users age.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log('Hello! My name is Aid.');
-console.log('I was created in 2024.');
+const input = require('sync-input');
+
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
+
+let name = input();
+
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+let remainder3 = Number(input());
+let remainder5 = Number(input());
+let remainder7 = Number(input());
+
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+console.log("Your age is " + age + "; that's a good time to start programming!");
